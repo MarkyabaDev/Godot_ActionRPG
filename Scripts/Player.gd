48788f7b1543541e3ce20a630a9070953d021cfa
Markus Baars <markus@baars42.de>
2022-02-14 12:53:23 +0100

    Added animation blend space 2d for correct animation with input vector

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -12,6 +12,8 @@ const FRICTION = 500
 # Onready var is only created if the Node (AnimationPlayer here) is ready.
 # Shortcut to the _ready() function
 onready var animationPlayer = $AnimationPlayer
+onready var animationTree = $AnimationTree
+onready var animationState = animationTree.get("parameters/playback")
 
 # Called when the node enters the scene tree for the first time. 
 # Similar to the Start() function in Unity.
@@ -30,15 +32,18 @@ func _physics_process(delta):
 	var inputVector = Vector2.ZERO
 	inputVector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
 	inputVector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
-	
+	inputVector = inputVector.normalized()
+
 	if inputVector != Vector2.ZERO:
-		if inputVector.x > 0:
-			animationPlayer.play("RunRight")
-		else:
-			animationPlayer.play("RunLeft")
+
+		# Needs to be here, because it snaps back to left.
+		animationTree.set("parameters/Idle/blend_position", inputVector)
+		animationTree.set("parameters/Run/blend_position", inputVector)
+
+		animationState.travel("Run")
 		velocity = velocity.move_toward(MAX_SPEED * inputVector.normalized(), ACCELERATION* delta)
 	else:
-		animationPlayer.play("IdleRight")
+		animationState.travel("Idle")
 		velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
 
 	velocity = move_and_slide(velocity)
