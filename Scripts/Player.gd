80f85a5a5ade35b07844bd18aa39006b52130ebe
Markus Baars <markus@baars42.de>
2022-02-14 12:03:22 +0100

    Added animations

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -9,25 +9,36 @@ const MAX_SPEED = 100
 const ACCELERATION = 500
 const FRICTION = 500
 
-# Called when the node enters the scene tree for the first time.
+# Onready var is only created if the Node (AnimationPlayer here) is ready.
+# Shortcut to the _ready() function
+onready var animationPlayer = $AnimationPlayer
+
+# Called when the node enters the scene tree for the first time. 
+# Similar to the Start() function in Unity.
 func _ready():
-	print("Hello world")
+	#animationPlayer = $AnimationPlayer
+	pass
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
+# Similar to the Update() function in Unity.
 #func _process(delta):
 #	pass
 
+# Similar to the FixedUpdate() function in Unity for physics calculation and movement.
 func _physics_process(delta):
 	var inputVector = Vector2.ZERO
 	inputVector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
 	inputVector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
-
+	
 	if inputVector != Vector2.ZERO:
-		velocity = velocity.move_toward(MAX_SPEED * inputVector, ACCELERATION* delta)
-		velocity += inputVector.normalized() * ACCELERATION * delta
-		velocity = velocity.clamped(MAX_SPEED)
+		if inputVector.x > 0:
+			animationPlayer.play("RunRight")
+		else:
+			animationPlayer.play("RunLeft")
+		velocity = velocity.move_toward(MAX_SPEED * inputVector.normalized(), ACCELERATION* delta)
 	else:
+		animationPlayer.play("IdleRight")
 		velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
 
 	velocity = move_and_slide(velocity)
