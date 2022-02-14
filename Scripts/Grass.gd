d567238e5e1fa4497f77197b366b288559d34344
Markus Baars <markus@baars42.de>
2022-02-14 16:49:30 +0100

    Added hit and hurt boxes
    Also changed the layer and masks for the different colliders.

--- Scripts/Grass.gd
+++ Scripts/Grass.gd
@@ -1,14 +1,12 @@
 extends Node2D
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	if Input.is_action_just_pressed("attack"):
+	
+func create_grass_effect():
 		var grassEffect = load("res://Scenes/Effects/GrassEffect.tscn")
 		var grassEffectInstance = grassEffect.instance()
 		var world = get_tree().current_scene
 		world.add_child(grassEffectInstance)
 		grassEffectInstance.global_position = global_position
 
-		# Similar to the Destroy() function in Unity. Queues the object for destruction, if nothing is accessing it.
-		queue_free()
-	
+func _on_Hurtbox_area_entered(area:Area2D):
+	create_grass_effect()
+	queue_free()
