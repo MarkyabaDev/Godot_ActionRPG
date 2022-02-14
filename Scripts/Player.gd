282e07203bec9a10871e0775ee50d818b0154068
Markus Baars <markus@baars42.de>
2022-02-14 14:13:46 +0100

    Added attack state and animations, with state change

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -4,10 +4,20 @@ extends KinematicBody2D
 # var a = 2
 # var b = "text"
 
-var velocity = Vector2.ZERO
 const MAX_SPEED = 100
 const ACCELERATION = 500
 const FRICTION = 500
+
+enum {
+	MOVE,
+	ROLL,
+	ATTACK
+}
+
+var velocity = Vector2.ZERO
+var state = MOVE
+
+
 
 # Onready var is only created if the Node (AnimationPlayer here) is ready.
 # Shortcut to the _ready() function
@@ -18,8 +28,7 @@ onready var animationState = animationTree.get("parameters/playback")
 # Called when the node enters the scene tree for the first time. 
 # Similar to the Start() function in Unity.
 func _ready():
-	#animationPlayer = $AnimationPlayer
-	pass
+	animationTree.active = true
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -29,6 +38,15 @@ func _ready():
 
 # Similar to the FixedUpdate() function in Unity for physics calculation and movement.
 func _physics_process(delta):
+	match state:
+		MOVE:
+			move_state(delta)
+		ROLL:
+			pass
+		ATTACK:
+			attack_state()
+
+func move_state(delta):
 	var inputVector = Vector2.ZERO
 	inputVector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
 	inputVector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
@@ -39,6 +57,7 @@ func _physics_process(delta):
 		# Needs to be here, because it snaps back to left.
 		animationTree.set("parameters/Idle/blend_position", inputVector)
 		animationTree.set("parameters/Run/blend_position", inputVector)
+		animationTree.set("parameters/Attack/blend_position", inputVector)
 
 		animationState.travel("Run")
 		velocity = velocity.move_toward(MAX_SPEED * inputVector.normalized(), ACCELERATION* delta)
@@ -47,3 +66,13 @@ func _physics_process(delta):
 		velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
 
 	velocity = move_and_slide(velocity)
+
+	if Input.is_action_just_pressed("attack"):
+		state = ATTACK
+
+func attack_state():
+	velocity = Vector2.ZERO
+	animationState.travel("Attack")
+
+func attack_animation_finished():
+	state = MOVE
